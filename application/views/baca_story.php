d8b5ce243e43cfda53a42b19cd91ce43c81a0139
haydariday <haydariday@gmail.com>
2019-04-22 04:37:50 +0700

    view
    pindahkan ke view

---
+++ application/views/baca_story.php
@@ -0,0 +1,60 @@
+<nav class="navbar navbar-light bg-white fixed-top fixed-top-2 border-bottom">
+        <div class="navbar-nav navbar-left ml-5">
+            <a href="#"><img class="pr-1" src="https://via.placeholder.com/28x43" width="28px" height="43px"></a>
+            <div class="d-inline">
+                <p class="mb-0 h5">Judul</p>
+                <span class="small">By name</span>
+            </div>
+        </div>
+        <div class="navbar-nav navbar-right ml-auto">
+            <div class="">
+                <button class="btn btn-secondary mr-3">
+                    Cancel
+                </button>
+            </div>
+        </div>
+        <div class="progress-container">
+                <div class="progress-bar" id="myBar"></div>
+        </div>
+    </nav>
+    <div class="container" style="margin-top: 200px">
+        <div class="text-center">
+                <h2>Judul</h2>
+                <div>
+                        <img src="https://via.placeholder.com/32x32" width="32" class="rounder-circle"><br>
+                        by <a href="#">nama</a>
+                </div>
+        </div>
+        <div class="m-5 p-5">
+                <p>Lorem ipsum dolor sit amet, amet noster torquatos vix at, in iuvaret eripuit qui. Duo an graeco commodo consetetur. Sed no inermis efficiendi disputando, an est error ignota constituam. Dicta omnesque reprimique ea sea, diam utinam veritus sit ne. Qui ei case dissentiunt, eam ut omnes facilisis, at vitae bonorum laoreet quo. Postea imperdiet argumentum eu quo, eu numquam platonem sea.
+                    </p>
+                    <p>
+                  Usu movet meliore voluptua ea, minim sonet iuvaret ut sed. Omnes fierent antiopam vis ex. At ius solet semper admodum. Ex quo nemore vulputate comprehensam, vel case putent saperet an, pri eros dicat nostrum cu. Ex accusamus conclusionemque nec, etiam eripuit incorrupte at est.
+                    </p>
+                    <p>
+                  Sit ut semper habemus mentitum, cibo tollit vocibus eu quo. Mei in utroque intellegam. Partem noluisse percipit ne eam, quot causae gloriatur nam ei. Eu augue dicant vis, in tation impedit quaerendum est, meis maiorum alienum ex nam. Sonet viris id vim, ei euismod lucilius duo.
+                    </p>
+                    <p>
+                  Nulla scripta an pro. Nec munere diceret legendos no, cibo novum quando et ius. Eam ea inani exerci. In vis falli utroque disputationi. Tollit aliquam recteque eu ius, quodsi legimus argumentum nec id, est esse inermis admodum in.
+                    </p>
+                    <p>
+                  Elitr phaedrum te mel, at sea simul accusamus. Has cu equidem deserunt mandamus, an sea suas eripuit probatus. Nam ei appareat sententiae, at civibus delectus quo. Dicam oporteat constituam id sed, ea dicam ridens eam.
+                    </p>
+                    <p>
+                  Aliquid percipit per cu. Vim munere offendit dignissim ad. Quo in denique pertinacia dissentias. Mollis tritani mei an, no esse nibh virtute mea.
+                    </p>
+                    <p>
+                  Est augue gloriatur ex, et vix iusto necessitatibus. Vel ea prima oratio, id nec movet corrumpit interpretaris, ex per vituperata necessitatibus. Molestie gloriatur vix cu. Pri assum ancillae cu. Eum at detraxit senserit euripidis, suas elaboraret vim ad. Commune pericula ius et.
+                    </p>
+                    <p>
+                  In choro everti utroque cum. Has stet deseruisse ea. Virtute inimicus et his. Sed ea nullam prompta delectus, constituto constituam ea pri. His sumo diceret an.
+                    </p>
+                    <p>
+                  In vix etiam mollis comprehensam. Amet deleniti qui cu, labores consequat ius id, quaeque saperet dissentiunt cu nec. Vitae honestatis pro no, duo at iusto omnesque, in usu lorem veritus. Iuvaret partiendo facilisis id pro, aeque disputationi delicatissimi ut quo. Sea ex meis nullam, nam amet placerat ea.
+                    </p>
+                    <p>
+                  Posidonium referrentur ea has. Ut vis accusamus constituto, ut mel graecis perfecto sadipscing. Honestatis appellantur ad est. Ei nam hinc splendide. Movet evertitur eum ut.
+                  </p>
+                  
+        </div>
+    </div>
---
+++ application/views/home.php
@@ -0,0 +1,207 @@
+<div id="HeroBox">
+		<div class="containerhome contenthome">
+			<div>
+				<h2 class="HWW">Hi, we're Wettped.</h2>
+				<h3 class="TWW">The world's most-loved social storytelling platform</h3>
+				<h4 class="WC">Wettped connects a global community of 70 million readers and writers through the power of story.</h4>
+				<div class="buttons">
+					<button class="btn btn-md btn-orange">Start Reading</button>
+					<button class="btn btn-md btn-orange">Start Writing</button>
+				</div>
+			</div>
+			<div class="imageContainer1">
+				<img src="img/W1.jpg" alt="hero-devices">
+			</div>
+		</div>
+	</div>
+	<div id="Intro">
+		<div class="containerhome contenthome">
+			<div class="imageContainer1">
+				<a href="https://after.wattpad.com" target="_blank" rel="noopener noreferrer">
+					<img src="img/promoposters.png" alt="after movie - promotional poster for the official after movie launch">
+				</a>
+			</div>
+			<div class="textContainer">
+				<h3 class="TWW">Your original story could be the next blockbuster</h3>
+				<p>Wettped Studios discovers untapped, unsigned, and talented writers on Wettped and connects them to global multi-media entertainment companies. 
+					<a href="/writers/" target="_blank">Find out more</a>
+				</p>
+				<div class="listContainer">
+					<h4>See Your Story...</h4>
+					<ul>
+						<li><img src="img/book.svg" alt="book">Get published</li>
+						<li><img src="img/video.svg" alt="reel">Get produced to movie or film</li>
+						<li><img src="img/tv.svg" alt="tv">Get adapted to a TV series</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div id="Logos">
+			<div class="containerhome">
+				<div id="publishers" class="logo-row">
+					<img src="img/logo1.svg" alt="sschuster">
+					<img src="img/logo2.svg" alt="hcollins">
+					<img src="img/logo3.svg" alt="macmillan">
+					<img src="img/logo4.svg" alt="wp_books">
+				</div>
+				<div id="studios" class="logo-row">
+					<img src="img/logo5.svg" alt="nbc">
+					<img src="img/logo6.svg" alt="paramount">
+					<img src="img/logo8.svg" alt="eone">
+					<img src="img/logo7.svg" alt="turner">
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="HowWattpadWorks">
+		<div class="container content">
+			<h2>How Wettped Works</h2>
+			<p>Get your story discovered through the power of community and technology on Wattpad.</p>
+			<div id="steps">
+				<div class="stepContainer">
+					<div class="step">
+						<h2 class="number">1</h2>
+						<div>
+							<h3 class="title">Create</h3>
+							<p class="description">Share your unique voice and original story on Wattpad. Find the writing resources you need to craft a story only you can tell.</p>
+						</div>
+					</div>
+					<div class="dots">
+						<span class="dot"></span>
+						<span class="dot"></span>
+						<span class="dot"></span>
+						<span class="dot"></span>
+						<span class="dot"></span>
+						<span class="dot"></span>
+					</div>
+					<div class="statBubble">
+						<div>
+							<div class="count">50+</div>
+							<div class="title">Writing Resources</div>
+						</div>
+					</div>
+				</div>
+				<div class="stepContainer">
+					<div class="step">
+						<h2 class="number">2</h2>
+						<div>
+							<h3 class="title">Build</h3>
+							<p class="description">Establish a global fan base as your story gains readership and momentum. Connect with other like-minded writers through storytelling.</p>
+						</div>
+					</div>
+					<div class="dots">
+						<span class="dot"></span>
+						<span class="dot"></span>
+						<span class="dot"></span>
+						<span class="dot"></span>
+						<span class="dot"></span>
+						<span class="dot"></span>
+					</div>
+					<div class="statBubble">
+						<div>
+							<div class="count">70 <span>MILLION</span></div>
+							<div class="title">Readers</div>
+						</div>
+					</div>
+				</div>
+				<div class="stepContainer">
+					<div class="step">
+						<h2 class="number">3</h2>
+						<div>
+							<h3 class="title">Amplify</h3>
+							<p class="description">Gain Wettped Star status and get your story published or adapted into film or television with Wattpad Studios and Wattpad Books!</p>
+						</div>
+					</div>
+					<div class="dots">
+						<span class="dot"></span>
+						<span class="dot"></span>
+						<span class="dot"></span>
+						<span class="dot"></span>
+						<span class="dot"></span>
+						<span class="dot"></span>
+					</div>
+					<div class="statBubble"><div>
+						<div class="count">1000+</div>
+						<div class="title">Story deals</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="Links">
+		<div id="GetDiscovered">
+			<div class="container">
+				<h3>Get Discovered</h3>
+				<div class="links">
+					<div class="link">
+						<div class="image">
+							<img src="img/dis1.png" alt="Writing Contests">
+						</div>
+						<div class="info">
+							<h4>Writing Contests</h4>
+							<p>Enter writing contests to get published, win awards, and partner with global brands.</p>
+						</div>
+					</div>
+					<div class="link">
+						<div class="image">
+							<img src="img/dis2.png" alt="The Wattys">
+						</div>
+						<div class="info">
+							<h4>The Wattys</h4>
+							<p>Wettped’s annual awards program committed to celebrating the best stories around the world.</p>
+						</div>
+					</div>
+					<div class="link">
+						<div class="image">
+							<img src="img/dis3.png" alt="Wattpad Picks">
+						</div>
+						<div class="info">
+							<h4>Wettped Picks</h4>
+							<p>Get featured on our hand-picked reading list.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="GetConnected">
+			<div class="container">
+				<h3>Get Connected</h3>
+				<div class="links">
+					<div class="link">
+						<div class="image">
+							<img src="img/dis4.png" alt="WattCon">
+						</div>
+						<div class="info">
+							<h4>WattCon</h4>
+							<p>Wettped’s premier annual writers’ conference offers support, mentorship, and career-oriented programming, to connect the Wattpad writer community, hone their craft, and grow their careers.</p>
+						</div>
+					</div>
+					<div class="link">
+						<div class="image">
+							<img src="img/dis5.png" alt="Community">
+						</div>
+						<div class="info">
+							<h4>Community</h4>
+							<p>Join Wettped Community to discuss shared interests and topics with other readers and writers.</p>
+						</div>
+					</div>
+					<div class="link">
+						<div class="image">
+							<img src="img/dis6.png" alt="Get Social">
+						</div>
+						<div class="info">
+							<h4>Get Social</h4>
+							<p>Connect with other passionate readers, writers, comment directly in stories as you read them, and support writers as they create and share their original stories.</p>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div id="TakeWP">
+		<div class="container content">
+			<h3>Take Wettped With You</h3>
+			<p>Read and write anywhere, even offline.</p>
+			<img src="img/GFooter.png" alt="devices" class="devices">
+		</div>
+	</div>
